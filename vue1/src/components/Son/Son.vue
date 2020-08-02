698245809b945b5b4e8a5758c14072e111f471b7
tiaozao <tiaozao@meizhuahuyu.com>
2020-08-03 00:01:50 +0800

    eslint 调整代码

---
+++ vue1/src/components/Son/Son.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Son">
+    <pre v-text="$attrs"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+  data () {
+    return {
+      'name': '111'
+    }
+  }
+}
+
+</script>
